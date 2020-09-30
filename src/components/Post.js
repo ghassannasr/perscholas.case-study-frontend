9833a5d720fe6a5607b9bfbe619ea5836bbd2ee2
Ghassan Nasr <ghassan.nasr@gmail.com>
2020-09-30 09:02:42 -0400

    Modularized the Post component code by creating methods to write out the JSX in smaller chunks.

--- src/components/Post.js
+++ src/components/Post.js
@@ -21,6 +21,10 @@ class Post extends React.Component {
     this.editPost = this.editPost.bind(this);
     this.savePost = this.savePost.bind(this);
     this.formatDate = this.formatDate.bind(this);
+    this.writePost = this.writePost.bind(this);
+    this.isAdminLoggedIn = this.isAdminLoggedIn.bind(this);
+    this.writeEditAndDeleteButtons = this.writeEditAndDeleteButtons.bind(this);
+    this.writeEditPostFormWithSaveButton = this.writeEditPostFormWithSaveButton.bind(this);
   }
 
   postBodyRef = React.createRef();
@@ -70,44 +74,62 @@ class Post extends React.Component {
     this.setState(state => ({ flag: "edit-post" }));
   }
 
+  writePost() {
+    return (
+      <>
+      <div>
+        <h2 className="blog-post-title">{this.state.postTitle}</h2>
+        <p className="blog-post-meta">
+          {this.formatDate(this.state.postDate)} by Ghassan Nasr
+                </p>
+        {renderHTML(this.state.postBody)}
+      </div>
+      {
+        this.isAdminLoggedIn() ? this.writeEditAndDeleteButtons() : ""
+      }
+      </>
+    );
+
+  }
+
+  isAdminLoggedIn() {
+    return(
+      this.props.login.adminIndex !== "" && this.props.login.adminIndex !== "error"
+    );
+  }
+
+  writeEditAndDeleteButtons() {
+    return(<>
+      <Button onClick={this.editPost} variant="outline-secondary">Edit</Button>
+      <Button value={this.state.postId} onClick={this.props.delete} variant="outline-secondary">Delete</Button>
+    </>
+);
+  }
+
+  writeEditPostFormWithSaveButton() {
+    return(
+    <Form >
+      <Form.Label>Blog Post Title:</Form.Label>
+      <Form.Control ref={this.postTitleRef} defaultValue={this.state.postTitle} type="text" ></Form.Control>
+      <Form.Label>Blog Post Body:</Form.Label>
+      <Form.Control ref={this.postBodyRef} as="textarea" defaultValue={this.state.postBody} type="text" >
+      </Form.Control>
+      <Button onClick={this.savePost} variant="outline-primary">Save</Button>
+    </Form>
+    );
+  }
 
   showPost() {
     return (
 
       <div className="col-sm-12 blog-main">
         <div className="blog-post">
-          {this.state.flag === "show-post" ?
-            <>
-              <div>
-                <h2 className="blog-post-title">{this.state.postTitle}</h2>
-                <p className="blog-post-meta">
-                    {this.formatDate(this.state.postDate)} by Ghassan Nasr
-                </p>
-                {renderHTML(this.state.postBody)}
-              </div>
-
-              {this.props.login.adminIndex !== "" && this.props.login.adminIndex !== "error"
-                ?
-                <>
-                  <Button onClick={this.editPost} variant="outline-secondary">Edit</Button>
-                  <Button value={this.state.postId} onClick={this.props.delete} variant="outline-secondary">Delete</Button>
-                </>
-
-                : ""
-              }
-
-            </>
-            : (this.state.flag === "edit-post" ?
-              <Form >
-                <Form.Label>Blog Post Title:</Form.Label>
-                <Form.Control ref={this.postTitleRef} defaultValue={this.state.postTitle} type="text" ></Form.Control>
-                <Form.Label>Blog Post Body:</Form.Label>
-                <Form.Control ref={this.postBodyRef} as="textarea" defaultValue={this.state.postBody} type="text" >
-                </Form.Control>
-                <Button onClick={this.savePost} variant="outline-primary">Save</Button>
-              </Form>
-              : {}
-            )
+          { 
+            this.state.flag === "show-post"
+              ?
+            this.writePost()
+              : 
+            this.state.flag === "edit-post" ? this.writeEditPostFormWithSaveButton() : {}
           }
         </div>
       </div>
